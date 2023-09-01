871b07b9fdaa0628023089c8ac9ac03f25e0b87d
whatthefoobar <irina.mntn@gmail.com>
2023-09-01 09:41:30 +0200

    added hue to header  (now happy enough)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <h1 className="header">Tomorrow's news</h1>;
+  return (
+    <h1 className="header">
+      Tomorrow's <span className="hue">news</span>
+    </h1>
+  );
 };
 
 export default Header;

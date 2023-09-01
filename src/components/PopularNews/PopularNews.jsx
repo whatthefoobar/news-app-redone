8f2362ffc2abf6d2e1fcf235b2c3e93297ce0909
whatthefoobar <irina.mntn@gmail.com>
2023-09-01 11:54:48 +0200

    enough of popular news for now

--- src/components/PopularNews/PopularNews.jsx
+++ src/components/PopularNews/PopularNews.jsx
@@ -9,16 +9,19 @@ const PopularNews = () => {
   return (
     <div>
       <h2>Popular News</h2>
-      <SinglePopularNews
-        author={author}
-        news={news1}
-        title={"Eskimos began to build hut"}
-      />
-      <SinglePopularNews
-        author={author}
-        news={news2}
-        title={"Ozone layer was repaired"}
-      />
+      <div className="popularNews">
+        <SinglePopularNews
+          author={author}
+          news={news1}
+          title={"Eskimos began to build hut"}
+        />
+        <SinglePopularNews
+          author={author}
+          news={news2}
+          title={"Ozone layer was repaired"}
+        />
+      </div>
+
       {/* pagination component */}
     </div>
   );

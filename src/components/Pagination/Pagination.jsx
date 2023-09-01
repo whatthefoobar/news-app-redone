c99773833c4b17c07034149a51ee2d700ca0c620
whatthefoobar <irina.mntn@gmail.com>
2023-09-01 18:08:08 +0200

    pagination elem added for popular news

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Pagination.css";
 
-const Pagination = () => {
-  return <div>Pagination</div>;
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  const pageNumbers = Array.from(
+    { length: totalPages },
+    (_, index) => index + 1
+  );
+
+  return (
+    <div className="pagination">
+      {pageNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          className={pageNumber === currentPage ? "active" : ""}
+          onClick={() => onPageChange(pageNumber)}
+        >
+          {pageNumber}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Pagination;
--- src/components/PopularNews/PopularNews.jsx
+++ src/components/PopularNews/PopularNews.jsx
@@ -1,30 +1,90 @@
-import React from "react";
-import "./PopularNews.css";
+import React, { useState } from "react";
 import SinglePopularNews from "../SinglePopularNews/SinglePopularNews";
+
 import author from "../../assets/images/author.jpg";
 import news1 from "../../assets/images/news1.jpg";
 import news2 from "../../assets/images/news2.jpeg";
+import news4 from "../../assets/images/news4.jpg";
+import news5 from "../../assets/images/news5.jpg";
+import news6 from "../../assets/images/news6.jpg";
+import Pagination from "../Pagination/Pagination";
 
 const PopularNews = () => {
+  // Sample news data
+  const data = {
+    news: [
+      {
+        author: author,
+        news: news1,
+        title: "Eskimos began to build hut",
+        imageDirection: "left",
+      },
+      {
+        author: author,
+        news: news2,
+        title: "Ozone layer was repaired",
+        imageDirection: "right",
+      },
+      {
+        author: author,
+        news: news4,
+        title: "Title3",
+        imageDirection: "left",
+      },
+      {
+        author: author,
+        news: news5,
+        title: "Title4",
+        imageDirection: "right",
+      },
+      {
+        author: author,
+        news: news6,
+        title: "Title5",
+        imageDirection: "left",
+      },
+      {
+        author: author,
+        news: news2,
+        title: "Title6",
+        imageDirection: "right",
+      },
+      // Add more news items here
+    ],
+  };
+
+  const itemsPerPage = 2; // Number of items per page
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.news.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="popularNews">
       <h2>Popular News</h2>
       <div className="container">
-        <SinglePopularNews
-          author={author}
-          news={news1}
-          title={"Eskimos began to build hut"}
-          imageDirection={"left"}
-        />
-        <SinglePopularNews
-          author={author}
-          news={news2}
-          title={"Ozone layer was repaired"}
-          imageDirection={"right"}
-        />
+        {currentItems.map((item, index) => (
+          <SinglePopularNews
+            key={index}
+            author={item.author}
+            news={item.news}
+            title={item.title}
+            imageDirection={item.imageDirection}
+          />
+        ))}
       </div>
 
-      {/* pagination component */}
+      {/* Pagination component */}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={Math.ceil(data.news.length / itemsPerPage)}
+        onPageChange={paginate}
+      />
     </div>
   );
 };

025e9ffad09d06cb578dc48daf79812a0b36a861
whatthefoobar <irina.mntn@gmail.com>
2023-11-20 14:43:16 +0100

    added Link to wrap whole article card

--- frontend/src/components/SinglePopularNews/SinglePopularNews.tsx
+++ frontend/src/components/SinglePopularNews/SinglePopularNews.tsx
@@ -21,28 +21,30 @@ const SinglePopularNews = ({
   published_date,
 }: IProps) => {
   return (
-    <div className="singlePopularNews">
-      <div className="singlePopularNews__image">
-        {imageSrc.length > 0 ? (
-          <img src={imageSrc} alt="news" />
-        ) : (
-          <img src={news} alt="news" />
-        )}
-      </div>
+    <Link to={`/news/${id}`}>
+      <div className="singlePopularNews">
+        <div className="singlePopularNews__image">
+          {imageSrc.length > 0 ? (
+            <img src={imageSrc} alt="news" />
+          ) : (
+            <img src={news} alt="news" />
+          )}
+        </div>
 
-      <div className="singlePopularNews__content">
-        <h4>{title}</h4>
-        <p>{newsContent}</p>
+        <div className="singlePopularNews__content">
+          <h4>{title}</h4>
+          <p>{newsContent}</p>
 
-        <p className="author">{byline}</p>
+          <p className="author">{byline}</p>
 
-        <Link to={`/news/${id}`}>Read more</Link>
+          <Link to={`/news/${id}`}>Read more</Link>
 
-        <div className="posted-date">
-          <p>{published_date}</p>
+          <div className="posted-date">
+            <p>{published_date}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 

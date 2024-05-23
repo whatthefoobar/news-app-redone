876d61843613871ab886de19035bfad92e685e7d
whatthefoobar <irina.mntn@gmail.com>
2024-05-23 14:10:47 +0200

    made simple Footer but commented it out from Layout for now

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,7 +3,25 @@ import "./Footer.css";
 
 const Footer = () => {
   const today = new Date();
-  return <div className="footer">Copyright &copy;{today.getFullYear()}</div>;
+  return (
+    <div className="footer">
+      <div className="column">
+        <strong>Today's News</strong>
+        <br />
+        Copyright &copy;{today.getFullYear()}
+      </div>
+      <div className="column">
+        <h4>London</h4>
+        <p>123 London Street</p>
+        <p>+44 123456789</p>
+      </div>
+      <div className="column">
+        <h4>Stockholm</h4>
+        <p>456 Stockholm Street</p>
+        <p>+46 987654321</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 import HeaderBar from "../HeaderBar/HeaderBar";
-import Footer from "../Footer/Footer";
+// import Footer from "../Footer/Footer";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -15,7 +15,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       <Header />
       <HeaderBar />
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };

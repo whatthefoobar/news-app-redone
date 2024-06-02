151dce0ef5d0eda8f947365a2106fc221384c474
whatthefoobar <irina.mntn@gmail.com>
2024-06-02 20:53:00 +0200

    a begining of a contact form

--- frontend/src/pages/ContactPage/ContactPage.tsx
+++ frontend/src/pages/ContactPage/ContactPage.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./ContactPage.css";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="contact-page">
+      <h1>Contact Us</h1>
+      <form className="contact-form">
+        <div className="form-field">
+          <input type="text" id="fullName" placeholder="Full Name" required />
+        </div>
+        <div className="form-field">
+          <input type="email" id="email" placeholder="Email" required />
+        </div>
+        <div className="form-field">
+          <textarea id="message" placeholder="Message" required></textarea>
+        </div>
+        <button className="contact-button" type="submit">
+          Contact Us
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactPage;

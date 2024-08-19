c03e1880a3526feafb670635bccca004c4e6b9ce
whatthefoobar <irina.mntn@gmail.com>
2024-08-19 12:07:52 +0200

    added simple loader gif

--- frontend/src/components/Loader/Loader.tsx
+++ frontend/src/components/Loader/Loader.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./Loader.css"; // Import the CSS for custom styling
+import spinner from "../../assets/images/spinner.gif"; // Adjust the path based on your folder structure
 
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="loader">
+      <img src={spinner} alt="Loading..." />
+    </div>
+  );
 };
 
 export default Loader;

106f0358460e2e1691bfb9bfff801c3c709c98ca
whatthefoobar <irina.mntn@gmail.com>
2024-08-20 14:30:30 +0200

    once form is submitted the form disapears and logs details

--- frontend/src/pages/ContactPage/ContactPage.tsx
+++ frontend/src/pages/ContactPage/ContactPage.tsx
@@ -1,24 +1,60 @@
-import React from "react";
+import { useState } from "react";
 import "./ContactPage.css";
 
 const ContactPage = () => {
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Full Name:", fullName);
+    console.log("Email:", email);
+    console.log("Message:", message);
+    setIsSubmitted(true);
+  };
   return (
     <div className="contact-page">
-      <form className="contact-form">
-        <h1>Contact Us</h1>
-        <div className="form-field">
-          <input type="text" id="fullName" placeholder="Full Name" required />
-        </div>
-        <div className="form-field">
-          <input type="email" id="email" placeholder="Email" required />
-        </div>
-        <div className="form-field">
-          <textarea id="message" placeholder="Message" required></textarea>
-        </div>
-        <button className="contact-button" type="submit">
-          Contact Us
-        </button>
-      </form>
+      {isSubmitted ? (
+        <h2>Thank you for your message!</h2>
+      ) : (
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <h1>Contact Us</h1>
+          <div className="form-field">
+            <input
+              type="text"
+              id="fullName"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-field">
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-field">
+            <textarea
+              id="message"
+              placeholder="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+          </div>
+          <button className="contact-button" type="submit">
+            Contact Us
+          </button>
+        </form>
+      )}
     </div>
   );
 };

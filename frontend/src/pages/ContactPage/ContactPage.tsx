5d2346d571c88f49e5f2d8b6da097149ec2302e1
whatthefoobar <irina.mntn@gmail.com>
2025-02-21 14:27:11 +0100

    for deployment

--- frontend/src/pages/ContactPage/ContactPage.tsx
+++ frontend/src/pages/ContactPage/ContactPage.tsx
@@ -15,13 +15,16 @@ const ContactPage = () => {
 
     // Send data to the backend
     try {
-      const response = await fetch("/api/contact", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ fullName, email, message }),
-      });
+      const response = await fetch(
+        "https://news-app-redone.vercel.app/api/contact",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ fullName, email, message }),
+        }
+      );
 
       if (response.ok) {
         setIsSubmitted(true);
